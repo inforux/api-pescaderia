62d7dcb0e8c77ac87ce21d2de178cd6ea535ed24
unknown <inforux@gmail.com>
2024-02-09 14:27:01 -0500

    soporte null para variables de entorno

--- src/db.ts
+++ src/db.ts
@@ -4,8 +4,8 @@ import { MONGO_URI } from "./config";
 export const connectDB = async () => {
   mongoose.set("strictQuery", true);
   try {
-    const conn = await mongoose.connect(MONGO_URI);
-    console.log(conn.connection.name);
+    const conn = mongoose.connect(MONGO_URI!); 
+    console.log((await conn).connection.name);
   } catch (error) {
     console.error(error);
     process.exit(1);
